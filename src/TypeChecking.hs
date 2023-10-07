c67b234090248ac72dc257d7bba2162665a5728e
Nick Chapman <nic@amadido.co.uk>
2023-10-07 09:45:48 +0100

    support stack & elem vars -- can type over-over example -- bye 350z :(

--- src/TypeChecking.hs
+++ src/TypeChecking.hs
@@ -29,7 +29,7 @@ extra = unlines
 tcMachine :: X.Machine -> IO ()
 tcMachine X.Machine{dispatchTable=dt,mem} = do
   let _all = [ x | (_,x) <- Map.toList userQDefs ]
-  mapM_ tcDef _all -- "'~23"
+  mapM_ tcDef "'~23"
   where
     tcDef :: Char -> IO ()
     tcDef c = do
@@ -102,7 +102,7 @@ tcSlotsExec = \case
 
 noTrans :: Infer Trans
 noTrans = do
-  x <- Fresh
+  x <- FreshS
   let s = S_Var x
   let m = Machine { stack = s }
   pure (T_Trans m m)
@@ -139,7 +139,7 @@ tcAddrExec a = case a of
 -- Language of Types
 
 data Scheme
-  = Scheme (Set SVar) Trans
+  = Scheme (Set SVar) (Set EVar) Trans
 
 -- Type of a machine tranformation -- what occurs during execution
 data Trans
@@ -152,19 +152,18 @@ data Machine = Machine
   -- TODO: and we need info relating to here/compiling
   }
 
--- Type of a stack of elements
+-- Type of a stack (of elements)
 data Stack
   = S_Cons Stack Elem
 --  | TS_empty
   | S_Skolem String -- existential state. you dont get to pick!
   | S_Var SVar -- (s1,s2,...)
 
--- Type of a stack-element (fits in a cell)
+-- Type of a stack-element (fits in one cell)
 data Elem
   = E_XT Trans
   | E_Numeric Numeric
---  | E_var Int -- TODO, need this (e1,e2,...)
--- Type of a stack of elements
+  | E_Var EVar -- (e1,e2,...)
 
 -- Type of thing that can b treated like a number
 data Numeric
@@ -178,13 +177,16 @@ data Contents
   | C_Elem Elem
 --  | C_Var Int -- (c1,c2...)
 
-data SVar = SVar Int -- currently only stacks can be variable
+data SVar = SVar Int
+  deriving (Eq,Ord)
+
+data EVar = EVar Int
   deriving (Eq,Ord)
 
 ----------------------------------------------------------------------
 -- Show
 
-instance Show Trans where -- TODO: rename Trans -> Trans (so can use T; keep E for Elem)
+instance Show Trans where
   show = \case
     T_Trans m1 m2 ->
       printf "(%s -- %s)" (show m1) (show m2)
@@ -195,14 +197,15 @@ instance Show Machine where
 
 instance Show Stack where
   show = \case
-    S_Cons s v -> printf "%s %s" (show s) (show v)
-    S_Var n -> show n
+    S_Cons s v -> printf "%s.%s" (show s) (show v)
     S_Skolem x -> x
+    S_Var v -> show v
 
 instance Show Elem where
   show = \case
     E_Numeric a -> show a
     E_XT e -> show e -- printf "XT%s" (show e)
+    E_Var v -> show v
 
 instance Show Numeric where
   show = \case
@@ -217,6 +220,9 @@ instance Show Contents where
 instance Show SVar where
   show (SVar n) = printf "s%s" (show n)
 
+instance Show EVar where
+  show (EVar n) = printf "e%s" (show n)
+
 ----------------------------------------------------------------------
 -- Primitives
 
@@ -229,10 +235,7 @@ schemeOfPrim = \case
 
   CompileComma -> scheme $ (s1 ~ xt (s2 ~~> s3)) ~~> s1
 
-  Over -> scheme $ (s1 ~ x1 ~ x2) ~~> (s1 ~ x1 ~ x2 ~ x1)
-    where
-      x1 = num -- Should be polymorphic. -- need polymorphic elem types
-      x2 = num
+  Over -> scheme $ (s1 ~ e1 ~ e2) ~~> (s1 ~ e1 ~ e2 ~ e1)
 
   _ -> Nothing
 
@@ -247,59 +250,104 @@ schemeOfPrim = \case
     xt = E_XT
     num = E_Numeric N_Number
 
-    s1 = mkVar 1
-    s2 = mkVar 2
-    s3 = mkVar 3
+    s1 = mkSVar 11
+    s2 = mkSVar 22
+    s3 = mkSVar 33
 
-    mkVar = S_Var . SVar
+    mkSVar = S_Var . SVar
+
+    e1 = mkEVar 44
+    e2 = mkEVar 55
+
+    mkEVar = E_Var . EVar
 
 
 makeScheme :: Trans -> Scheme
-makeScheme t = Scheme (varsOfTrans t) t
+makeScheme t = Scheme (svarsOfTrans t) (evarsOfTrans t) t
 
 tcPrimExec :: Prim -> Infer Trans
 tcPrimExec prim =
   case schemeOfPrim prim of
     Nothing -> Nope (printf "tcPrimExec: %s" (show prim))
-    Just scheme -> instantiateScheme scheme
+    Just scheme -> do
+      t <- instantiateScheme scheme
+      --Message (printf "%s:: %s" (show prim) (show t))
+      pure t
 
 instantiateScheme :: Scheme -> Infer Trans
-instantiateScheme (Scheme vars ty) = do
-  bs <- sequence [ do y <- Fresh; pure (x,S_Var y) | x <- Set.toList vars]
-  let sub = Subst (Map.fromList bs)
+instantiateScheme (Scheme svars evars ty) = do
+  s <- Map.fromList <$> sequence [ do y <- FreshS; pure (x,S_Var y)
+                                 | x <- Set.toList svars ]
+  e <- Map.fromList <$> sequence [ do y <- FreshE; pure (x,E_Var y)
+                                 | x <- Set.toList evars ]
+  let sub = Subst { s , e }
   pure (subTrans sub ty)
 
 ----------------------------------------------------------------------
--- varsOf*
+-- svarsOf*
 
-varsOfTrans :: Trans -> Set SVar
-varsOfTrans = \case
-  T_Trans m1 m2 -> varsOfMachine m1 `Set.union` varsOfMachine m2
+svarsOfTrans :: Trans -> Set SVar
+svarsOfTrans = \case
+  T_Trans m1 m2 -> svarsOfMachine m1 `Set.union` svarsOfMachine m2
 
-varsOfMachine :: Machine -> Set SVar
-varsOfMachine = \case
-  Machine{stack} -> varsOfStack stack
+svarsOfMachine :: Machine -> Set SVar
+svarsOfMachine = \case
+  Machine{stack} -> svarsOfStack stack
 
-varsOfStack :: Stack -> Set SVar
-varsOfStack = \case
-  S_Cons s e -> varsOfStack s `Set.union` varsOfElem e
-  S_Var x -> Set.singleton x
+svarsOfStack :: Stack -> Set SVar
+svarsOfStack = \case
+  S_Cons s e -> svarsOfStack s `Set.union` svarsOfElem e
+  S_Var x -> Set.singleton x -- collect here
   S_Skolem{} -> Set.empty
 
-varsOfElem :: Elem -> Set SVar -- TODO rename suffic Elem -> Elem
-varsOfElem = \case
-  E_Numeric n -> varsOfNumeric n
-  E_XT t -> varsOfTrans t
+svarsOfElem :: Elem -> Set SVar
+svarsOfElem = \case
+  E_Numeric n -> svarsOfNumeric n
+  E_XT t -> svarsOfTrans t
+  E_Var{} -> Set.empty
 
-varsOfNumeric :: Numeric -> Set SVar
-varsOfNumeric = \case
+svarsOfNumeric :: Numeric -> Set SVar
+svarsOfNumeric = \case
   N_Number -> Set.empty
-  N_Address c -> varsOfContents c
+  N_Address c -> svarsOfContents c
 
-varsOfContents :: Contents -> Set SVar
-varsOfContents = \case
+svarsOfContents :: Contents -> Set SVar
+svarsOfContents = \case
   C_Char -> Set.empty
-  C_Elem e -> varsOfElem e
+  C_Elem e -> svarsOfElem e
+
+----------------------------------------------------------------------
+-- evarsOf*
+
+evarsOfTrans :: Trans -> Set EVar
+evarsOfTrans = \case
+  T_Trans m1 m2 -> evarsOfMachine m1 `Set.union` evarsOfMachine m2
+
+evarsOfMachine :: Machine -> Set EVar
+evarsOfMachine = \case
+  Machine{stack} -> evarsOfStack stack
+
+evarsOfStack :: Stack -> Set EVar
+evarsOfStack = \case
+  S_Cons s e -> evarsOfStack s `Set.union` evarsOfElem e
+  S_Var{} -> Set.empty
+  S_Skolem{} -> Set.empty
+
+evarsOfElem :: Elem -> Set EVar
+evarsOfElem = \case
+  E_Numeric n -> evarsOfNumeric n
+  E_XT t -> evarsOfTrans t
+  E_Var x -> Set.singleton x -- collect here
+
+evarsOfNumeric :: Numeric -> Set EVar
+evarsOfNumeric = \case
+  N_Number -> Set.empty
+  N_Address c -> evarsOfContents c
+
+evarsOfContents :: Contents -> Set EVar
+evarsOfContents = \case
+  C_Char -> Set.empty
+  C_Elem e -> evarsOfElem e
 
 ----------------------------------------------------------------------
 -- sub*
@@ -322,7 +370,7 @@ subStack sub = loop
       S_Cons s v ->
         S_Cons (loop s) (subElem sub v)
       stack@(S_Var var) ->
-        case applySubst sub var of
+        case applySubstS sub var of
           Nothing -> stack
           Just replacement -> replacement
       stack@S_Skolem{} ->
@@ -332,6 +380,10 @@ subElem :: Subst -> Elem -> Elem
 subElem sub = \case
   E_Numeric n -> E_Numeric (subNumeric sub n)
   E_XT t -> E_XT (subTrans sub t)
+  elem@(E_Var var) ->
+    case applySubstE sub var of
+      Nothing -> elem
+      Just replacement -> replacement
 
 subNumeric :: Subst -> Numeric -> Numeric
 subNumeric sub = \case
@@ -364,13 +416,13 @@ unifyStack s1 s2 =
   case (s1,s2) of
 
     (S_Var x1, stack@(S_Var x2)) ->
-      if x1==x2 then pure () else InfSubst x1 stack
+      if x1==x2 then pure () else SubStack x1 stack
 
     (S_Var x, stack) ->
-      if x `elem` varsOfStack stack then cyclic else InfSubst x stack
+      if x `elem` svarsOfStack stack then cyclic else SubStack x stack
 
     (stack, S_Var x) ->
-      if x `elem` varsOfStack stack then cyclic else InfSubst x stack
+      if x `elem` svarsOfStack stack then cyclic else SubStack x stack
 
     (S_Cons s1 e1, S_Cons s2 e2) -> do
       unifyStack s1 s2
@@ -382,11 +434,21 @@ unifyStack s1 s2 =
 
   where
     nope = Nope (printf "unifyStack: %s ~ %s" (show s1) (show s2))
-    cyclic = Nope (printf "cyclic: %s =!= %s" (show s1) (show s2))
+    cyclic = Nope (printf "cyclic: %s ~ %s" (show s1) (show s2))
 
 unifyElem :: Elem -> Elem -> Infer ()
 unifyElem e1 e2 =
   case (e1,e2) of
+
+    (E_Var x1, el@(E_Var x2)) ->
+      if x1==x2 then pure () else SubElem x1 el
+
+    (E_Var x, el) ->
+      if x `elem` evarsOfElem el then cyclic else SubElem x el
+
+    (el, E_Var x) ->
+      if x `elem` evarsOfElem el then cyclic else SubElem x el
+
     (E_Numeric n1, E_Numeric n2) -> unifyNumeric n1 n2
     (E_XT t1, E_XT t2) -> unifyTrans t1 t2
 
@@ -394,6 +456,7 @@ unifyElem e1 e2 =
     (E_XT{},E_Numeric{}) -> nope
   where
     nope = Nope (printf "unifyElem: %s ~ %s" (show e1) (show e2))
+    cyclic = Nope (printf "cyclic: %s ~ %s" (show e1) (show e2))
 
 unifyNumeric :: Numeric -> Numeric -> Infer ()
 unifyNumeric a1 a2 =
@@ -414,16 +477,19 @@ unifyContents = undefined -- TODO!
 -- Infer
 
 instance Functor Infer where fmap = liftM
-instance Applicative Infer where pure = InfReturn; (<*>) = ap
-instance Monad Infer where (>>=) = InfBind
+instance Applicative Infer where pure = Return; (<*>) = ap
+instance Monad Infer where (>>=) = Bind
 
 data Infer a where
-  InfReturn :: a -> Infer a
-  InfBind :: Infer a -> (a -> Infer b) -> Infer b
-  InfSubst :: SVar -> Stack -> Infer ()
+  Return :: a -> Infer a
+  Bind :: Infer a -> (a -> Infer b) -> Infer b
+  Message :: String -> Infer ()
+  SubStack :: SVar -> Stack -> Infer ()
+  SubElem :: EVar -> Elem -> Infer ()
   Nope :: String -> Infer a
   CurrentSub :: Infer Subst
-  Fresh :: Infer SVar
+  FreshS :: Infer SVar
+  FreshE :: Infer EVar
 
 
 type InfRes a = IO (Either TypeError a)
@@ -438,14 +504,24 @@ runInfer inf0 = loop state0 inf0 k0
 
     loop :: State -> Infer a -> (a -> State -> InfRes b) -> InfRes b
     loop s inf k = case inf of
-      InfReturn x -> do
+      Return x -> do
         k x s
-      InfBind m f -> do
+      Bind m f -> do
         loop s m $ \a s -> loop s (f a) k
-      InfSubst v stack -> do
-        --printf "%s -> %s\n" (show v) (show stack)
+      Message mes -> do
+        printf "runInfer: %s\n" mes
+        k () s
+      SubStack v stack -> do
+        --printf "SubStack: %s -> %s\n" (show v) (show stack)
         let State{subst} = s
-        let subst' = extendSubst subst v stack
+        let subst' = extendSubstStack subst v stack
+        --printf "subst: %s\n" (show subst')
+        checkInvariant subst'
+        k () s { subst = subst' }
+      SubElem v elem -> do
+        --printf "SubElem: %s -> %s\n" (show v) (show elem)
+        let State{subst} = s
+        let subst' = extendSubstElem subst v elem
         --printf "subst: %s\n" (show subst')
         checkInvariant subst'
         k () s { subst = subst' }
@@ -454,9 +530,13 @@ runInfer inf0 = loop state0 inf0 k0
       CurrentSub -> do
         let State{subst} = s
         k subst s
-      Fresh -> do
+      FreshS -> do
         let State{u} = s
         let x = SVar u
+        k x s { u = u + 1 }
+      FreshE -> do
+        let State{u} = s
+        let x = EVar u
         k x s { u = u + 1 }
 
 data State = State { subst :: Subst, u :: Int }
@@ -467,18 +547,24 @@ state0 = State { subst = subst0, u = 0 }
 ----------------------------------------------------------------------
 -- Subst
 
-data Subst = Subst (Map SVar Stack) -- TODO: Need 2nd map for non stack vars
+data Subst = Subst
+  { s :: Map SVar Stack
+  , e :: Map EVar Elem
+  }
 
-applySubst :: Subst -> SVar -> Maybe Stack
-applySubst (Subst m) x = Map.lookup x m
+applySubstS :: Subst -> SVar -> Maybe Stack
+applySubstS Subst {s} x = Map.lookup x s
+
+applySubstE :: Subst -> EVar -> Maybe Elem
+applySubstE Subst {e} x = Map.lookup x e
 
 domain :: Subst -> Set SVar
-domain (Subst m) = Set.fromList $ Map.keys m
+domain Subst{s} = Set.fromList $ Map.keys s
 
 range :: Subst -> Set SVar
-range (Subst m) = Set.unions [ varsOfStack s | s <- Map.elems m ]
+range Subst{s} = Set.unions [ svarsOfStack v | v <- Map.elems s ]
 
-checkInvariant :: Subst -> IO ()
+checkInvariant :: Subst -> IO () -- TODO: invariant for all kinds of vars
 checkInvariant sub = do
   let d = domain sub
   let r = range sub
@@ -491,24 +577,26 @@ checkInvariant sub = do
     error "invariant fails"
 
 instance Show Subst where
-  show (Subst m) =
-    unwords [ printf "%s: %s," (show v) (show s)
-            | (v,s) <- Map.toList m ]
+  show Subst{s,e} =
+    unwords $
+    [ printf "%s: %s," (show k) (show v) | (k,v) <- Map.toList s ] ++
+    [ printf "%s: %s," (show k) (show v) | (k,v) <- Map.toList e ]
 
 subst0 :: Subst
-subst0 = Subst Map.empty
+subst0 = Subst { s = Map.empty, e = Map.empty }
 
-extendSubst :: Subst -> SVar -> Stack -> Subst
-extendSubst (Subst m) key replacement = do
+extendSubstStack :: Subst -> SVar -> Stack -> Subst
+extendSubstStack Subst {s, e} key replacement = do
+  let sub = Subst { s = Map.singleton key replacement, e = Map.empty }
+  Subst { s = Map.insert key replacement (Map.map (subStack sub) s)
+        , e = Map.map (subElem sub) e }
 
-  let g = Subst (Map.singleton key replacement)
-  let mShifted = Map.map (subStack g) m
-  Subst (Map.insert key replacement mShifted)
-
--- TODO: need a 2nd type for vars which can bind to value-types
--- How name things to make it clear when have value/stack vars ?
+extendSubstElem :: Subst -> EVar -> Elem -> Subst
+extendSubstElem Subst {s, e} key replacement = do
+  let sub = Subst { e = Map.singleton key replacement, s = Map.empty }
+  Subst { s = Map.map (subStack sub) s
+        , e = Map.insert key replacement (Map.map (subElem sub) e) }
 
 data TypeError = TypeError String
 
 deriving instance Show TypeError
-
---
+++ thoughts.md
@@ -0,0 +1,11 @@
+
+freshen skolen vars - for less unification / stricter checking
+
+stratify elem space:
+    x elem = xt|numeric(n)
+    n numeric = number|address(a)
+    a address = of-char|of-elem(x)
+
+vars at muti levels: elem,numeric,address (as well as stack)
+
+litx/litn/lita - so can preserve/recover types in gen code
